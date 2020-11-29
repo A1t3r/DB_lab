627c89c3e5632148b58401a5c3b081e4cec3e264
PoliakovVI <valerider@icloud.com>
2020-11-29 16:49:38 +0300

    class database was added

---
+++ source/database.py
@@ -0,0 +1,31 @@
+class Database:
+    def __init__(self, url):
+        pass
+
+    def create_database(self):
+        pass
+
+    def delete_database(self):
+        pass
+
+    def create_tables(self):
+        pass
+
+    def get_table(self, name):
+        pass
+
+    def clear_table(self, name):
+        pass
+
+    # функция частичной очистки одной из таблиц???
+
+    def insert_into(self, table_name, values):
+        pass
+
+    # Поиск по заранее выбранному(вами) текстовому не ключевому полю
+
+    # Обновление кортежа
+
+    # Удаление по заранее выбранному текстовому не ключевому полю
+
+    # Удаление конкретной записи, выбранной пользователем
