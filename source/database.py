f00cfef33f62ae6ec6d010aca88b63769142c05c
A1t3r <korkunov_a@list.ru>
2020-11-29 21:55:59 +0300

    накатил методов

--- source/database.py
+++ source/database.py
@@ -1,41 +1,49 @@
 class user:
-    def __init__(self, name,  password):
-        self.name=name
-        self.password=password
+    def __init__(self, name, password):
+        self.name = name
+        self.password = password
 
     def get_user_information(self):
         return self.name + " " + self.password
+
 
 class Database:
     def __init__(self, url):
         pass
 
-    def create_database(self):
+    def create_database(self):  # создание бд
         pass
 
-    def delete_database(self):
+    def delete_database(self):  # удаление бд
         pass
 
-    def create_tables(self):
+    def create_tables(self):  # создание таблицы
         pass
 
-    def get_table(self, name):
+    def get_table(self, name):  # вывод содержимого таблицы
         pass
 
-    def clear_table(self, name):
+    def clear_table(self, name):  # это частичная (очистили одну)
         pass
 
-    # функция частичной очистки одной из таблиц???
-
-    def insert_into(self, table_name, values):
+    def clear_all_table(self, name):  # это полная (очистили все)
+        # for table in tables:
+        #   clear_table(table)
         pass
 
-    # Поиск по заранее выбранному(вами) текстовому не ключевому полю
+    # функция частичной очистки одной из таблиц??? - частичная, значит очистить одну таблицу
+    # а полная значит очистить все таблицы
 
-    # Обновление кортежа
+    def insert_into(self, table_name, values):  # добавление данных
+        pass
 
-    # Удаление по заранее выбранному текстовому не ключевому полю
+    def search_by_group(self, group_name):# Поиск по заранее выбранному(вами) текстовому не ключевому полю
+        pass  # мы вроде решили, что ищем по названию группы
 
-    # Удаление конкретной записи, выбранной пользователем
+    def update_table(self, table):# Обновление кортежа
+        pass
 
-
+    def delete_by_group(self, group_name):# Удаление по заранее выбранному текстовому не ключевому полю
+        pass
+    def single_delete(self, record_id):# Удаление конкретной записи, выбранной пользователем
+        pass
