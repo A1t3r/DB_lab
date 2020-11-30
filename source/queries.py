80f175e0a6a1b5c6871b55e9b83a0ad6debabb01
PoliakovVI <valerider@icloud.com>
2020-11-30 12:02:30 +0300

    table queries were added

--- source/queries.py
+++ source/queries.py
@@ -1,24 +1,57 @@
 table_names = frozenset(('Student', 'Group', 'Schedule', 'Course'))
 
 create_table_student_query = """
-to do
+create table Student
+(
+	id serial primary key,
+	groupID integer,
+	name varchar(10),
+	surname varchar(15),
+	classes_number integer,
+	foreign key (groupID) references Group (id)
+);
 """
 
 create_table_group_query = """
-to do
+create table Group
+(
+	id serial primary key,
+	name string
+);
 """
 
 create_table_schedule_query = """
-to do
-"""
+create table Schedule
+(
+	groupID integer,
+	weekday string,
+	time time,
+	courseID integer,
+	audience varchar(4),
+	lecturer varchar(20)
+	constraint couple primary key(group, weekday, time),
+	foreign key (courseID) references Course (id)
+);
+"""  # !!!!!!!!!!!!!!!!!!!!!! тип поля time: time / numeric
 
 create_table_course_query = """
-to do
+create table Course
+(
+	id serial primary key,
+	name varchar(40)
+);
 """
 
 
 def select_all_from(table_name):
     if table_name in table_names:
         return "select * from {}".format(table_name)
+    else:
+        raise ValueError("There is no table with name '{}'".format(table_name))
+
+
+def delete_all_from(table_name):
+    if table_name in table_names:
+        return "delete from {}".format(table_name)
     else:
         raise ValueError("There is no table with name '{}'".format(table_name))
