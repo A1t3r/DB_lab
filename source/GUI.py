16f52292e16637cc4d0803202461900a0a172e7d
PoliakovVI <valerider@icloud.com>
2020-12-07 19:31:33 +0300

    разработан каркас ряда базовых вызовов и функций

--- source/GUI.py
+++ source/GUI.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox as mb
 from database import Database
 
 import variables as v
@@ -7,19 +8,76 @@ import variables as v
 def create_database():
     pass
 
-def detele_database():
+
+def create_database_def():
     pass
 
-def fill_tables():
+
+def create_database_window():
+    window = Toplevel(root)
+    window.title("Create database")
+    entries = dict()
+
+    num = 0
+    for i in range(len(v.cd_names)):
+        Label(window, padx=v.cd_pad, pady=v.cd_pad, text=v.cd_names[i], width=v.cd_label_width).grid(row=i, column=0)
+        ent = Entry(window, width=v.cd_entry_width)
+        ent.grid(row=i, column=1, padx=v.cd_pad, pady=v.cd_pad)
+        entries['ent1'] = ent
+        num += 1
+
+    Button(window, text="Create def", command=create_database_def).grid(row=num,
+                                                                        column=0)  # might be deleted when p'll be finished
+    Button(window, text="Create", command=create_database).grid(row=num, column=1, padx=v.cd_pad, pady=v.cd_pad)
+
+
+def connect_database():
     pass
+
+
+def connect_database_def():
+    pass
+
+
+def connect_database_window():
+    window = Toplevel(root)
+    window.title("Connect database")
+    entries = dict()
+
+    num = 0
+    for i in range(len(v.cd_names)):
+        Label(window, padx=v.cd_pad, pady=v.cd_pad, text=v.cd_names[i], width=v.cd_label_width).grid(row=i, column=0)
+        ent = Entry(window, width=v.cd_entry_width)
+        ent.grid(row=i, column=1, padx=v.cd_pad, pady=v.cd_pad)
+        entries['ent1'] = ent
+        num += 1
+
+    Button(window, text="Connect def", command=connect_database_def).grid(row=num,
+                                                                          column=0)  # might be deleted when p'll be finished
+    Button(window, text="Connect", command=connect_database).grid(row=num, column=1, padx=v.cd_pad, pady=v.cd_pad)
+
+
+def detele_database():
+    answer = mb.askyesno(
+        title="Attention",
+        message="Do you want to delete database?")
+    if answer:
+        pass  # there have to be deleting database
+
+
+def fill_tables():
+    pass  # there have to be database filling with data
+
 
 def clear_tables():
     pass
 
+
 def pack_menu():
     db_menu = Menu()
-    db_menu.add_command(label="Create database", command=create_database)
-    db_menu.add_command(label="Delete database", command=detele_database)
+    db_menu.add_command(label="Create", command=create_database_window)
+    db_menu.add_command(label="Connect", command=connect_database_window)
+    db_menu.add_command(label="Delete", command=detele_database)
 
     table_menu = Menu()
     table_menu.add_command(label="Fill tables", command=fill_tables)
@@ -33,25 +91,33 @@ def pack_menu():
 
 
 def root_settings():
+    root.title("The most impressive application name")
+
+
+def show_tables_tool():
     pass
 
 
-def pack_frame_data():
-    frame_data.pack(side=LEFT)
-
-
-def pack_frame_tools():
-    frame_tools.pack()
-
-
+# root
 root = Tk()
 root_settings()
 pack_menu()
 
-frame_data = Frame(root, width=v.width_frame_data, height=v.height_frame_data, bg="darkred")
-pack_frame_data()
+# database
+database = None
 
-frame_tools = Frame(root, width=v.width_frame_tools, height=v.height_frame_tools, bg="green", bd=20)
-pack_frame_tools()
+# show panel
+main_lbox = Listbox(width=v.width_listbox, height=v.height_listbox)
+main_lbox.pack(side=LEFT)
+
+# tools panel
+tools_label = Label(text="tools", width=v.width_tools, bg="red")
+tools_label.pack(side=TOP)
+
+tool_frame = Frame(root, bg='cyan')
+tool_frame.pack(side=TOP)
+
+show_tables_tool()
+
 
 root.mainloop()
--- source/variables.py
+++ source/variables.py
@@ -2,10 +2,22 @@
 width_root = 600
 height_root = 400
 
-# frame data
-width_frame_data = 600
-height_frame_data = 400
+# listbox
+width_listbox = 100
+height_listbox = 20
 
-# frame tools
-width_frame_tools = 200
-height_frame_tools = 400
+# tools
+width_tools = 25
+
+# database
+base_bd_name = 'postgres'
+bd_name = 'kekv'
+db_username = 'postgres'
+db_password = '123'
+host = 'localhost'
+
+# create database
+cd_label_width = 15
+cd_entry_width = 20
+cd_names = ['Base db', 'Username', 'Password', 'Host']
+cd_pad = 10
